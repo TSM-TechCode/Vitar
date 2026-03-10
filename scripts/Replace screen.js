198dcbe1c962b7a208e16d5b854990a1cb134464
TSM-Script <contaquenaoprestaserio@gmail.com>
2026-03-10 15:17:41 -0300

    Create Replace screen.js

---
+++ scripts/Replace screen.js
@@ -0,0 +1,6 @@
+let excelBasico = document.querySelector(".excelBasico");
+
+excelBasico.onclick = function ExibirexcelBasico() {
+  document.querySelector(".other").style.display = "none";
+  document.querySelector("#ExcelBasico").style.display = "block";
+  }
