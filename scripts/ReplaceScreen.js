14379658c0ab0e080f959aeb2684cc0f853083b3
TSM-Script <contaquenaoprestaserio@gmail.com>
2026-03-16 13:41:16 -0300

    Update 1.0 ReplaceScreen.js
    Essa atualizaçao trás a opçao onde voçê possa fechar a aba sem precisar atualizar o site.

--- scripts/ReplaceScreen.js
+++ scripts/ReplaceScreen.js
@@ -1,6 +1,26 @@
+let vezesTroca = 0;
 let excelBasico = document.querySelector(".excelBasico");
 
-excelBasico.onclick = function ExibirexcelBasico() {
-  document.querySelector(".other").style.display = "none";
-  document.querySelector("#ExcelBasico").style.display = "block";
+excelBasico.onclick = function exibirExcelBasico() {
+  
+  const habilitar = function habilitarExcelBasico() {
+
+    document.querySelector(".other").style.display = "none";
+    document.querySelector("#ExcelBasico").style.display = "block";
   }
+
+  const desabilitar = function desabilitarExcelBasico() {
+    document.querySelector("#ExcelBasico").style.display = "none";
+    document.querySelector(".other").style.display = "block";
+  }
+  if (vezesTroca === 0) {
+    habilitar();
+    vezesTroca = vezesTroca + 1;
+  }
+  else if (vezesTroca > 0) {
+    desabilitar();
+    vezesTroca = vezesTroca - 1;
+  } else {
+    console.log("Ocorreu algum erro!");
+  }
+      }
