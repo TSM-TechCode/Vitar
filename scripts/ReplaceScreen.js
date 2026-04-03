8edc21d2047feaf2fe29bf3f964ece9effd78873
TSM-Script <contaquenaoprestaserio@gmail.com>
2026-04-03 13:26:05 -0300

    Update ReplaceScreen.js
    Correção de bug...

--- scripts/ReplaceScreen.js
+++ scripts/ReplaceScreen.js
@@ -1,26 +1,31 @@
+let excelBasico = document.querySelector('.excelBasico');
+
 let vezesTroca = 0;
-let excelBasico = document.querySelector(".excelBasico");
 
 excelBasico.onclick = function exibirExcelBasico() {
   
   const habilitar = function habilitarExcelBasico() {
 
-    document.querySelector(".other").style.display = "none";
-    document.querySelector("#ExcelBasico").style.display = "block";
+    document.querySelector('.navbar').style.display = 'none';
+    document.querySelector('.secHome').style.display = 'none';
+    document.querySelector('.secSobre').style.display = 'none';
+    document.querySelector('#ExcelBasico').style.display = 'block';
   }
 
   const desabilitar = function desabilitarExcelBasico() {
-    document.querySelector("#ExcelBasico").style.display = "none";
-    document.querySelector(".other").style.display = "block";
+    document.querySelector('#ExcelBasico').style.display = 'none';
+    document.querySelector('.navbar').style.display = 'block';
+    document.querySelector('.secHome').style.display = 'block';
+    document.querySelector('.secSobre').style.display = 'block';
   }
   if (vezesTroca === 0) {
     habilitar();
-    vezesTroca = vezesTroca + 1;
+    vezesTroca += 1;
   }
   else if (vezesTroca > 0) {
     desabilitar();
-    vezesTroca = vezesTroca - 1;
+    vezesTroca = 0;
   } else {
-    console.log("Ocorreu algum erro!");
+    console.log('Ocorreu algum erro!');
   }
-      }
+}
